0a264349754b69f252c1e3802c2f7a08ca336990
vittoria <vpags1@yahoo.com>
2018-08-01 19:58:04 -0500

    bad password

--- bad.php
+++ bad.php
@@ -60,7 +60,6 @@ padding: 12px 20px;
   <center>
   <div class="main-container">
     <div class="form">
-          <h1><font size= "7">Are you sure you belong here?</h1></font>
           <h1><font size= "7">Do you even belong here?</h1></font>
 
           <img src="images/morty.jpg" width="600" height="539" alt="pinch" />
